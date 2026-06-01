3270255ea4c6d919853527e6a73baecfdee6867d
Meghan Denny <hello@nektro.net>
2026-06-01 16:13:39 -0700

    update to zig 0.15.2

--- build.zig
+++ build.zig
@@ -3,14 +3,16 @@ const deps = @import("./deps.zig");
 
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
-    const mode = b.option(std.builtin.Mode, "mode", "") orelse .Debug;
+    const mode = b.option(std.builtin.OptimizeMode, "mode", "") orelse .Debug;
     const disable_llvm = b.option(bool, "disable_llvm", "use the non-llvm zig codegen") orelse false;
 
     const exe = b.addExecutable(.{
         .name = "zig-inquirer",
-        .root_source_file = b.path("src/main.zig"),
-        .target = target,
-        .optimize = mode,
+        .root_module = b.createModule(.{
+            .root_source_file = b.path("src/main.zig"),
+            .target = target,
+            .optimize = mode,
+        }),
     });
     deps.addAllTo(exe);
     exe.use_llvm = !disable_llvm;
@@ -27,9 +29,11 @@ pub fn build(b: *std.Build) void {
     run_step.dependOn(&run_cmd.step);
 
     const tests = b.addTest(.{
-        .root_source_file = b.path("test.zig"),
-        .target = target,
-        .optimize = mode,
+        .root_module = b.createModule(.{
+            .root_source_file = b.path("test.zig"),
+            .target = target,
+            .optimize = mode,
+        }),
     });
     deps.addAllTo(tests);
     tests.use_llvm = !disable_llvm;
